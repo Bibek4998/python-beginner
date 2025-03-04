1fbe7c2be1f8113349809ca48dab1e8b61e697eb
Bibek4998 <bibekjoshi07@gmail.com>
2025-03-04 13:35:19 +0545

    Wrote a code to shut down the pc

---
+++ shutdown.py
@@ -0,0 +1,4 @@
+# The code to shutdown the pc ...
+import os
+os.system("shutdown /s /t 5") #Te character 's' in this code mean shut down and the character 't' mean time delay
+# If you run the above code the computer will shutdown in 5 seconds ..
