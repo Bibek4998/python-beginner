d7e8eda5a3655105cb4ab5a9e5d100bcaa174f48
Bibek4998 <bibekjoshi07@gmail.com>
2025-03-24 14:59:29 +0545

    Created the class file and made an example of the class

---
+++ class_ex.py
@@ -0,0 +1,5 @@
+# This is the class example in python programming.
+class MyClass: # this is the class name.
+    txt="Hello this is the demo text." # this is the class variable.
+text = MyClass() # this is the object of the class.
+print(text.txt) # this is the output of the class example.
