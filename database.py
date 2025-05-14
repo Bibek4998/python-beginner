2a1fca34a1fb6fa02b44537e55340247fa8d4dc9
Bibek4998 <bibekjoshi07@gmail.com>
2025-05-14 07:40:49 +0545

    Updated the whole database file code to make it look more understandable

--- database.py
+++ database.py
@@ -1,12 +1,35 @@
 import sqlite3
-conn=sqlite3.connect("test.db")
+conn=sqlite3.connect("Database.db")
 cursor=conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS USERS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, AGE INTEGER NOT NULL)")
-cursor.execute("INSERT INTO USERS (NAME, AGE) VALUES (?,?)", ('Bibek', 16))
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS students(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    semester INTEGER,
+    batch INTEGER
+    )
+"""
+)
+cursor.executemany(
+    "INSERT INTO students(id,name,semester,batch) VALUES(?,?,?,?)",
+    [
+        (1,"Jhon David",5,2021),
+        (2,"David Bekham",8,2015),
+        (3,"Rudra Shakya",8,2017),
+        (4,"Srijan Sact",1,2025),
+        (5,"Avi David",1,2024),
+        (6,"Shown Crint",6,2019),
+        (7,"Sujan Poudel",2,2023),
+        (8,"Ram BK",5,2021),
+        (9,"Shyam Sharma",5,2020),
+        (10,"Spura Kantra",3,2022)
+    ]
+)
 conn.commit()
-def view():
-    cursor.execute("SELECT * FROM USERS")
-    rows=cursor.fetchall()
-    for row in rows:
-        print(row)
-view()
+cursor.execute("SELECT * FROM students WHERE name LIKE 'S%a' COLLATE NOCASE")
+rows=cursor.fetchall()
+for row in rows:
+    print("ID: ",row[0],"Name: ",row[1],"Semester: ",row[2],"Batch:",row[3])
+cursor.close()
+conn.close()
